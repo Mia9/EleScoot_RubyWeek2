09b0af8bb06fd2970bea3dab2f09933ad6b2b2ad
Lim <limhsiuying90@gmail.com>
2023-08-25 11:55:55 +0800

    2nd commit

--- ScooterWk2.rb
+++ ScooterWk2.rb
@@ -4,34 +4,58 @@
 #Calculation (time, rental_fee)
 #Docking Station (location and capacity-parking spaces)
 
-Class Rider
+class Rider
 attr_reader :name, :contact_number
-	def initialize
+	def initialize(name, contact_number)
 		@name = name
 	    @contact_number = contact_number	
 	end
 end
 
-Class Scooter
-attr_reader :scooter_id	
-	def initialize
-		@scooter_id = scooter_id	
-	end
-end
-
-Class Station
-
+class Station
 attr_reader :station_name, :capacity
-	def initialize
+	def initialize(station_name, capacity)
 		@station_name = station_name
 		@capacity = 0
 	end
 end
 
-# Class Calculation #later
 
-# def rent_time
-	
-# end
+class Scooter  #available or not
+attr_reader :scooter_id, :state
+	def initialize(scooter_id, state)
+		@scooter_id = scooter_id
+        @state = state	
+	end
 
-# end
+	def state_status
+	   @state = "available"
+	end
+end
+
+# class Calculation #later
+
+rider = Rider.new("Mia", "0113346")
+puts "Hi! Welcome to Elescoot! What is your name?"
+puts "#{rider.name}"
+puts "What's your phone number?"
+puts "#{rider.contact_number}"
+puts "-------"
+
+station = Station.new("KK", 0)
+puts "Which station are you right now?"
+puts "#{station.station_name}"
+puts "-------"
+
+scooter = Scooter.new("Scoot01", "available")
+puts "This #{scooter.scooter_id} is #{scooter.state_status}."
+
+
+
+
+
+
+
+
+
+
